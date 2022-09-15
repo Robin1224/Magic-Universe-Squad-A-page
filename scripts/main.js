77aa5c911f2326873e268921b55a3724c25dae5d
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2022-09-15 09:24:15 +0200

    toggle and begin of responsive layout

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,16 @@
 function delay(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
-
+ 
 // Functie voor het omdraaien van alle kaarten (wordt geroepen door de knop)
 async function turnCards() {
   // Eén voor één alle kaarten langs om ze te laten draaien
+  let button = document.getElementById("turn-button");
+  let fill = document.getElementById("toggle-fill");
+
+  button.classList.toggle("on")
+  fill.classList.toggle("toggled");
+
   for (let i = 1; i <= 25; i++) {
     let card = document.getElementById(i);
 
